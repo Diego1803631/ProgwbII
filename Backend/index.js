4ef7b38755b2eea3a141f6a8bcbaf7ce68c6683f
Diego Flores <diego1803631@gmail.com>
2021-09-08 20:57:05 -0500

    Usuario agregado

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,26 @@ const port = 5001;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+require('./src/models/connection');
+const User = require('./src/models/user');
+
 app.get('/', (req, res) => {
     res.send('Success!!');
+});
+
+app.post('/user', (req, res) => {
+    let newUser = new User({
+        name: 'Diego',
+        lastname: 'Flores',
+        username: 'master1234',
+        type: 1,
+        email: 'diego@gmail.com',
+        password: '1234',
+        age: 21
+    });
+    newUser.save().then((newDBObject) => console.log('success', newDBObject))
+    .catch((err) => console.log('error', err));
+    res.send(newUser);
 });
 
 app.listen(port, () => {
