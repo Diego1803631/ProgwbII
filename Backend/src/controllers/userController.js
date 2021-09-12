a69684d411bd8271dcc6b9c272b87677727bdae1
Diego Flores <diego1803631@gmail.com>
2021-09-12 12:06:01 -0500

    Update y Delete agregados

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,16 +1,37 @@
 const User = require('../models/userSchema');
 
+exports.user_create = async (req, res) => {
+    //validations
+    const { body } = req;
 
-exports.user_create = (req, res) => {
-    let newUser = new User({
-        name: 'Juan',
-        lastname: 'Flores',
-        username: 'master1234',
-        type: 1,
-        email: 'diego@gmail.com',
-        password: '1234',
-        age: 21
-    });
-    newUser.save().then((newDBObject) => console.log('success', newDBObject)).catch((err) => console.log('error', err));
+    let newUser = new User(body);
+    await newUser.save().then((newDBObject) => console.log('success', newDBObject)).catch((err) => console.log('error', err));
+
     res.send(newUser);
+};
+
+exports.user_getall = async (req, res) => {
+    const data = await User.find();
+    res.send(data);
+};
+
+exports.user_getbyid = async (req, res) => {
+    const { id } = req.params;
+    const data = User.findById(id);
+    res.send();
+};
+
+exports.user_update = async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    const data = await User.findOneAndUpdate({ _id: id }, body);
+    res.send(data);
+};
+
+exports.user_delete = async (req, res) => {
+    const { id } = req.params;
+    await User.deleteOne({ _id: id });
+
+    res.send({ message: 'User deleted' });
 };
